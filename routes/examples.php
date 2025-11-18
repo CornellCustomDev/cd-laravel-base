c8872604e446c955ca7be1d8a873ef038d0d4688
Eric Woods <woodseowl@gmail.com>
2025-11-18 12:21:40 -0500

    Implement CU Auth for examples

--- routes/examples.php
+++ routes/examples.php
@@ -1,8 +1,12 @@
 <?php
 
-Route::get('/examples/cds', function () {
-    return view('examples/cds');
-})->name('examples/cds');
-Route::get('/examples/form', function () {
-    return view('examples/form');
-})->name('examples/form');
+use CornellCustomDev\LaravelStarterKit\CUAuth\Middleware\CUAuth;
+
+Route::group(['middleware' => [CUAuth::class]], function() {
+    Route::get('/examples/cds', function () {
+        return view('examples/cds');
+    })->name('examples/cds');
+    Route::get('/examples/form', function () {
+        return view('examples/form');
+    })->name('examples/form');
+});
