df3a1245f3dbf418f3175adeedcd615ed36e4168
Melissa Sweeney <msc18@cornell.edu>
2025-11-25 09:36:03 -0500

    remove commented out code in callout component

--- resources/views/components/cds/callout-error.blade.php
+++ resources/views/components/cds/callout-error.blade.php
@@ -1,12 +1,8 @@
 @props([
-    // 'variant' => 'danger',
-    // 'color' => 'red',
     'icon' => 'exclamation-triangle',
 ])
 
 <flux:callout
-    {{-- variant="{{ $variant }}" --}}
-    {{-- color="{{ $color }}" --}}
     icon="{{ $icon }}"
     {{ $attributes }}
 >
