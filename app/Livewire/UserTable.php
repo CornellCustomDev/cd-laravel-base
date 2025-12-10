f3850bc6a32d4e270ba26e482c08adc3d12bb6f4
Irina Naydich <ivn2@cornell.edu>
2025-12-10 18:12:09 -0500

    reset default selected user to null

--- app/Livewire/UserTable.php
+++ app/Livewire/UserTable.php
@@ -19,7 +19,7 @@ class UserTable extends Component
 
     public $allUsers;
     public $selectedUser;
-    public $selectedUserId = 3;
+    public $selectedUserId = null;   //you can set a default user id here
     public $sortBy = 'name';
     public $sortDirection = 'desc';
 
